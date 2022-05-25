78198451b449f85bd2a9d474f93001dd831269e0
Gedvidas <gedvidas.lapienis@gmail.com>
2022-05-25 17:12:18 +0300

    Fixing bugs

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -78,7 +78,7 @@ class UserController
         $this->validateRequired($request, 'pass1', 'Slaptazodis neivestas', true);
         $this->validateRequired($request, 'pass2', 'Slaptazodio pakartojimas neivestas', true);
 
-        $this->validateValidUsername($request, 'username', 'Ivesti neleistini simboliai', true);
+        $this->validateValidUsername($request, 'username', 'Ivesti neleistini simboliai');
         $this->validateValidEmail($request, 'email', 'Ivesti neleistini simboliai');
         $this->validateValidPass($request, 'pass1', 'Ivesti neleistini simboliai');
 
@@ -156,7 +156,7 @@ class UserController
             return false;
         }
 
-        if (!preg_match('@[A-Z]@', $request->getBody()[$name])) {
+        if (!preg_match('/^\w{2,20}$/', $request->getBody()[$name])) {
             Application::$errors[$name] = $error;
             return false;
         }
